0036252ee0d95609acc0f27008ea989344f75d70
Szymon Dolata <dodo@MacBook-Air-Szymon.local>
2023-03-18 14:11:19 +0100

    springboot init

---
+++ src/main/java/com/mizuhogroup/inteview/InteviewApplication.java
@@ -0,0 +1,13 @@
+package com.mizuhogroup.inteview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InteviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InteviewApplication.class, args);
+	}
+
+}
